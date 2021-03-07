239219f367cffe15e3289bc9f596de81e465b1e2
David Kirakosyan <kirakosyandaviddev@gmail.com>
2021-03-07 20:38:27 +0400

    update base url

--- src/app/Api.js
+++ src/app/Api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseUrl = 'https://api.github.com/search/';
+
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: baseUrl,
     timeout: 1000,
   });
 
